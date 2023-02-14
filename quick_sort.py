31d5ba69da52af065da2a4818144c465b47e2545
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-02-14 18:36:47 +0300

    add quick_sort algorithm

---
+++ quick_sort.py
@@ -0,0 +1,31 @@
+import random
+from Validators_.sorting_validator import validate_array_of_integers
+
+
+@validate_array_of_integers(parameter='list')
+def quick_sorted(array_: list[int], classic: bool = True) -> list[int]:
+    """
+    Quick sort algorithm that doesn't sort original array, it creates new object based sort it and return.
+    Time complexity is based on pivot. In classic realization pivot every time is a random element.
+                                       So at most it'll be O(n log n), worst case O(n^2)
+    Space complexity is O(n), sometimes its O(log n)
+    :param array_: list[int]
+    :param classic: bool
+    :return: list[int]
+    """
+    if len(array_) < 2:
+        return array_
+    current_index = random.randint(0, len(array_) - 1) if classic else 0
+    pivot = array_[current_index]
+    left_side = [number for number in array_[:current_index] + array_[current_index + 1:] if number <= pivot]
+    right_side = [number for number in array_[:current_index] + array_[current_index + 1:] if number > pivot]
+    return quick_sorted(left_side) + [pivot] + quick_sorted(right_side)
+
+
+def main():
+    print(quick_sorted([2, 1, 0, -33, 20, 423, -221, 0]))
+    assert quick_sorted([2, 1, 0]) == [0, 1, 2]
+
+
+if __name__ == '__main__':
+    main()
