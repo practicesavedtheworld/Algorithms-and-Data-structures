59b51d12be44de7711a26c640458484aaf021c7d
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-02-16 12:22:23 +0300

    add graph reader

---
+++ graph_reader.py
@@ -0,0 +1,10 @@
+def graph_reader(**kwargs: list[str]) -> dict[str, list[str]]:
+    """
+    Create a dictionary based on given keyword arguments.
+    First parameter is auto assumed  as 'start'. So the first node that you send into function defined
+    by the function as start.
+    :param kwargs: list[str]
+    :return:dict[str, list[str]]
+    """
+    dependency_graph = kwargs
+    return dependency_graph
