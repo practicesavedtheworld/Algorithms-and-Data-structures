b90fefc0372b1731fadcf3d4c7156594557265ef
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-02-20 17:04:22 +0300

    Add weighted_graph_reader first_version

--- graph_reader.py
+++ graph_reader.py
@@ -1,4 +1,8 @@
+from collections import namedtuple
+from typing import Optional
 from Validators_ import graph_validator
+
+WeightedGraph = namedtuple('WeightedGraph', 'graph costs parents')
 
 
 @graph_validator.validate_graph
@@ -12,3 +16,22 @@ def graph_reader(**kwargs: list[str]) -> dict[str, list[str]]:
     """
     dependency_graph = kwargs
     return dependency_graph
+
+
+def weighted_graph_reader(*all_vertexes: str, **weights: int) -> WeightedGraph:
+    """
+
+    :param all_vertexes: str
+    :param weights: int
+    :return: WeightedGraph
+    """
+    vertexes: dict[str, dict] = {element: dict() for element in all_vertexes}
+    costs = {vertex: float('inf') if vertex != 'start' else 0 for vertex in vertexes}
+    parents: dict[str, Optional[str]] = {vertex: None for vertex in vertexes if vertex != 'start'}
+    for vertex, weight in weights.items():
+        start_point, end_point = vertex.split('_')
+        vertexes[start_point][end_point] = weight
+    for key, value in vertexes['start'].items():
+        costs[key] = value
+        parents[key] = 'start'
+    return WeightedGraph(vertexes, costs, parents)
