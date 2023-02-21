c9e33b1acec3078be18ef8662dd72e1e004f7d17
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-02-21 13:37:47 +0300

    add exceptions for graphs

--- Exceptions_/exceptions_graph.py
+++ Exceptions_/exceptions_graph.py
@@ -4,6 +4,28 @@ class IncorrectStartingNodeName(Exception):
 For example start=['1', '2', '3']"""
 
 
+class ExcludedStart(Exception):
+    def __str__(self):
+        return "There is no 'start' node in graph or its called incorrect. Add your starting point as 'start'"
+
+
+class ExcludedFinish(Exception):
+    def __str__(self):
+        return "There is no 'fin' node in graph or its called incorrect. Add your final point called as 'fin'"
+
+
+class NegativeWeightInGraph(Exception):
+    def __str__(self):
+        return "That realization doesn't work with negative weights pathing. Use graph with positive weights"
+
+
+class MissingNodeSeparator(Exception):
+    def __str__(self):
+        return """Path from point to point must be separated by the '_' symbol.
+For e.g path from A to B that weights 14 handle like this  A_B=14, provided that A is not the starting vertex.
+Starting vertex must be called 'start'"""
+
+
 class WrongNeighborsType(Exception):
     def __str__(self):
         return f"""Neighbor nodes(element) must be in list-type
