f49c3fe217e2905a5e72fb46228c7003f4a56e8d
practicesavedtheworld <richdaysiscoming@gmail.com>
2024-03-02 12:12:30 +0300

    add .h file

---
+++ c++/sorting_algorithms/bubble_sort.h
@@ -0,0 +1,11 @@
+//
+// Created by bdc on 02.03.2024.
+//
+
+#ifndef ALGOS_BUBBLE_SORT_H
+#define ALGOS_BUBBLE_SORT_H
+
+#endif //ALGOS_BUBBLE_SORT_H
+#include <cstdint>
+
+void bubbleSort(int64_t array[], int array_size);
