651d82cbde5621a8ffe500d5785f04a48700ee88
practicesavedtheworld <richdaysiscoming@gmail.com>
2024-03-07 16:48:19 +0300

    remove unused, add cstdlib for clear types

--- c++/sorting_algorithms/bubble_sort.h
+++ c++/sorting_algorithms/bubble_sort.h
@@ -1,11 +1,3 @@
-//
-// Created by bdc on 02.03.2024.
-//
-
-#ifndef ALGOS_BUBBLE_SORT_H
-#define ALGOS_BUBBLE_SORT_H
-
-#endif //ALGOS_BUBBLE_SORT_H
-#include <cstdint>
+#include <cstdlib>
 
 void bubbleSort(int64_t array[], int array_size);
