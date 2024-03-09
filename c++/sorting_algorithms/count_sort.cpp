8e3593f2f7ed02faa09e88acadd45179b2847470
practicesavedtheworld <richdaysiscoming@gmail.com>
2024-03-09 11:40:44 +0300

    add realization of count sort

---
+++ c++/sorting_algorithms/count_sort.cpp
@@ -0,0 +1,30 @@
+#include "count_sort.h"
+#include <map>
+
+
+namespace CountSort
+{
+    // Using ordered map
+    void countSort(int64_t *arr, size_t arr_size) {
+        //  Time complexity: O(n)
+        //  Space complexity: O(n)
+        std::map<int64_t, int64_t> number_counter;
+        for (int i = 0; i < arr_size; i++)
+        {
+            auto current_element = arr[i];
+            if (number_counter.find(current_element) != number_counter.end())
+            {
+                number_counter[current_element] += 1;
+            }
+            else number_counter[current_element] = 1;
+        }
+
+        int idx = 0;
+        for (auto &pair: number_counter)
+            for (int i = 0; i < pair.second; i++)
+            {
+                arr[idx++] = pair.first;
+            }
+
+    }
+} // namespace CountSort
