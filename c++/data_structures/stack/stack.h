3cc4e6a0b6e958c2b9c4abb484f1e04c6d8e238a
practicesavedtheworld <richdaysiscoming@gmail.com>
2024-03-12 11:19:28 +0300

    add header for stack

---
+++ c++/data_structures/stack/stack.h
@@ -0,0 +1,24 @@
+#include <cstdint>
+
+namespace CustomStack
+{
+    /*
+    Data structure that store elements in an array. It works
+    like a stack in a LIFO order. So, it lets us push and pop elements.
+    Pop and push are O(1) operations.
+     */
+    class ArrayBasedStack
+    {
+    public:
+        uint64_t limit;
+        int top;
+        int64_t * storage = nullptr;
+
+        explicit ArrayBasedStack(uint64_t limit);
+
+        void push(int64_t element);
+
+        int64_t pop();
+    };
+
+} // namespace CustomStack
