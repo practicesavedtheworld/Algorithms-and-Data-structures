b1f28d79dcfee121815e71f5112f4bc7c336a719
practicesavedtheworld <richdaysiscoming@gmail.com>
2024-03-15 12:10:38 +0300

    add header file

---
+++ c++/algorithms/sorting_algorithms/selection_sort.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace SelectionSort
+{   /*
+     * Selection sort algorithm
+     * The idea is to find the minimum element in the array and swap it with the current element
+     */
+    void selectionSort(int64_t arr[], size_t arr_size);
+} // namespace SelectionSort
