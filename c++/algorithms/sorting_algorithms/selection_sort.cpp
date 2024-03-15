2127322d67f9f8a39d34274fe193723529e2353a
practicesavedtheworld <richdaysiscoming@gmail.com>
2024-03-15 12:10:56 +0300

    add selection sort realization

---
+++ c++/algorithms/sorting_algorithms/selection_sort.cpp
@@ -0,0 +1,20 @@
+#include "selection_sort.h"
+
+
+namespace SelectionSort
+{
+    void selectionSort(int64_t arr[], size_t arr_size) {
+
+        // Time complexity: O(N^2). Worst case: O(N^2)
+        // Space complexity: O(1)
+        for (int pos = 0; pos < arr_size - 1; pos++) {
+            int min = pos;
+            for (int i = pos + 1; i < arr_size; i++) {
+                if (arr[i] < arr[min]) min = i;
+            }
+            int64_t tmp = arr[pos];
+            arr[pos] = arr[min];
+            arr[min] = tmp;
+        }
+    }
+} // namespace SelectionSort
